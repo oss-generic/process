5d89d0ed4b20427c5cc2ad51912297adbb0b4b13
Aaron Chong <aaronjhchong@hotmail.com>
2016-12-17 09:22:00 +0800

    [#1] Fix ToC of coding standards (#6)
    * Move duplicate function to markdown.js
    * Move preprocessing to proper function
    * Remove prefix added by GitHub markdown API
    * Auto generate anchors for markdown headers

--- scripts/markdown.js
+++ scripts/markdown.js
@@ -10,15 +10,12 @@ $.ajax({
             url: "https://api.github.com/markdown",
             data: JSON.stringify({
                 "text": data,
-                "mode": "gfm"
+                "mode": "markdown"
             }),
             error: function(jqXHR, textStatus, error) {
                 redirectToGithub();
             },
             success: function(data) {
-                data = data.replace(/<th align="center">Good<\/th>/g, '<th align="center" class="example-good">Good</th>');
-                data = data.replace(/<th align="center">Bad<\/th>/g, '<th align="center" class="example-bad">Bad</th>');
-                data = data.replace('id="user-content-borderless"', 'class="borderless"');
                 processAndDisplayResult(data);
             }
         });
@@ -31,4 +28,17 @@ function redirectToGithub() {
 
 function displayResult(html) {
     $(document.body).html($('<div class="markdown-body">' + html + '</div>'));
+}
+
+function processAndDisplayResult(html) {
+    // Pre-process the result as necessary
+    html = html.replace(/user-content-/g, '');
+    html = html.replace(/<th align="center">Good<\/th>/g, '<th align="center" class="example-good">Good</th>');
+    html = html.replace(/<th align="center">Bad<\/th>/g, '<th align="center" class="example-bad">Bad</th>');
+    html = html.replace('id="borderless"', 'class="borderless"');
+
+    // Display the result in the page; this is the minimum requirement of the function
+    displayResult(html);
+
+    // Post-process the result as necessary
 }

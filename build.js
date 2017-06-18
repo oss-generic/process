443585b689428c53e293683805de426c1bc19a05
Paul Tan <pyokagan@gmail.com>
2017-06-19 01:13:06 +0800

    Setup asciidoc-to-html build system
    Our markdown documentation is published to GitHub pages via its
    automatic markdown files publishing support[1]. However, this mechanism
    does not support asciidoc files.
    
    We are in the process of converting all of our documentation to
    asciidoc, so we'll need to setup our own GitHub pages build and
    deployment system.
    
    Let's setup up our asciidoc-to-html build and deployment system as
    follows:
    
    * Build system: asciidoctor.js, on top of the nodejs platform.
      The nodejs platform was chosen because it is likely the oss-generic
      developers would be familiar with it and have it already installed on
      their system.
    
      For simplicity, we'll write a custom build script (build.js) for
      build automation. With a simple `npm run build`, the build script will
      build the documentation into the `dist` directory.
    
      The build system setup requires us to remove the HTML/CSS files which
      implement the existing "render markdown via javascript"
      implementation. Otherwise, the HTML files would override the generated
      HTML files from the asciidoc documentation.
    
    * Deployment system: Travis CI. Set Travis up via the .travis.yml file
      such that upon a push to master, Travis will automatically build the
      documentation using `npm run build` and publish the built
      documentation to GitHub Pages.
    
      Travis was chosen because it has built-in support for deploying to
      GitHub pages, and it is already used in oss-generic related projects
      like TEAMMATES and SE-EDU so developers should already be familiar
      with it.
    
    [1] https://github.com/blog/2289-publishing-with-github-pages-now-as-easy-as-1-2-3

---
+++ build.js
@@ -0,0 +1,99 @@
+'use strict';
+const assert = require('assert');
+const path = require('path');
+const asciidoctor = require('asciidoctor.js')();
+const fs = require('fs-extra');
+const glob = require('glob');
+
+/** Directory where asciidoctor stylesheets are stored. */
+const stylesDir = path.join(__dirname, 'styles');
+/** Destination directory. */
+const destDir = 'dist';
+/** `glob` paths to ignore */
+const pathsToIgnore = [
+    'node_modules',
+    'node_modules/**',
+    `${destDir}`,
+    `${destDir}/**`,
+    'package.json',
+];
+
+if (require.main === module) {
+    fs.remove(destDir).then(buildAllFiles).catch(onError);
+}
+
+/**
+ * Builds all files in the current directory and all of its subdirectories.
+ */
+async function buildAllFiles() {
+    const filenames = glob.sync('**/*', {
+        ignore: pathsToIgnore,
+        nodir: true,
+    });
+
+    for (const filename of filenames) {
+        await buildFile(filename);
+    }
+}
+
+/**
+ * Builds a file.
+ *
+ * @param {string} filename - The file to build. Must not be an ignored file.
+ */
+async function buildFile(filename) {
+    if (filename.endsWith('.adoc')) {
+        await asciidocToHtml(filename);
+    } else {
+        await copyMediaFile(filename);
+    }
+}
+
+/**
+ * Renders an asciidoc file into a HTML file.
+ *
+ * @param {string} adocFilename - The asciidoc file to render.
+ */
+async function asciidocToHtml(adocFilename) {
+    console.log('asciidocToHtml', adocFilename);
+    const relativeStylesDir = path.relative(path.dirname(adocFilename), stylesDir);
+    const asocInput = await fs.readFile(adocFilename, 'utf8');
+    const htmlOutput = asciidoctor.convert(asocInput, {
+        safe: 'safe',
+        doctype: 'article',
+        header_footer: true,
+        attributes: [
+            'linkcss',
+            `stylesdir=${relativeStylesDir}`,
+            'experimental',
+            'sectlinks',
+            'idprefix=',
+            'idseparator=-',
+            'source-highlighter=highlightjs',
+        ],
+        base_dir: path.dirname(adocFilename),
+    });
+    const outputFilename = path.join(destDir, path.dirname(adocFilename), path.basename(adocFilename, '.adoc')) + '.html';
+    await fs.mkdirs(path.dirname(outputFilename));
+    await fs.writeFile(outputFilename, htmlOutput);
+}
+
+/**
+ * Copy a media file to the destination directory.
+ *
+ * @param {string} mediaFilename - Media filename to copy.
+ */
+async function copyMediaFile(mediaFilename) {
+    console.log('copyMediaFile', mediaFilename);
+    const outputFilename = path.join(destDir, mediaFilename);
+    await fs.mkdirs(path.dirname(outputFilename));
+    await fs.copy(mediaFilename, outputFilename);
+}
+
+/**
+ * Fatal error handler.
+ */
+function onError(err) {
+    console.error(err);
+    process.exit(1);
+}
